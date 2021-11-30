3024c7fad5345e95de9b3057e92e307bcc4bd035
Percy <percy.quezada@pucp.pe>
2021-11-30 11:22:54 -0500

    initial commit

---
+++ src/main/java/ws/synospis/trainingpercyquezada/TrainingPercyQuezadaApplication.java
@@ -0,0 +1,13 @@
+package ws.synospis.trainingpercyquezada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPercyQuezadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPercyQuezadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/synospis/trainingpercyquezada/TrainingPercyQuezadaApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.synospis.trainingpercyquezada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPercyQuezadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
